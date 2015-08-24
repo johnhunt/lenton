ead681abeb8bee5ca7db64f67f9f67abca5fe798
Alex Lenton <lenton64@gmail.com>
2015-08-24 11:23:36 +0100

    Add screenshots for Webplates

--- app/pages/webplates.php
+++ app/pages/webplates.php
@@ -2,4 +2,15 @@
 
 <p>Webplates was an e-commerce which allowed web developers to sell their templates online. Developers uploaded their compressed files for review and if ok, would be published on the website. This project was great experience for building an e-commerce system and communicating with a payment gateway API (on this occasion; PayPal IPN).</p>
 
-<i>I'm still working on getting Webplates working again to take screenshots of it. They will be added here once they are taken.</i>
+<?php
+$screenshots = [
+    'home.png',
+    'view_template.png',
+    'developer_portal.png',
+    'selling_info.png',
+];
+?>
+
+<?php foreach ($screenshots as $screenshot): ?>
+    <img src="<?=URL_ROOT?>images/screenshots/webplates/<?=$screenshot?>" alt="" /><br /><br />
+<?php endforeach ?>
